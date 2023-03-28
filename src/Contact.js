d0986dc2c4c1223b87788ed89639e740b6c4cece
Jitendra Choudhary <jituchoudhary20@gmail.com>
2023-03-28 12:34:22 +0530

    footer added

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,5 @@
 import React from "react";
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
-import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
+
 import "./CSS/Contact.css";
 import "./index.css";
 import HeroImg2 from "./HeroImg2";
@@ -43,49 +41,6 @@ console.log(err));
 
       </div>
 
-
-
-
-
-      <div className="contact-info">
-        <h1>Contact</h1>
-        <div className="contact">
-          <LocalPhoneOutlinedIcon />
-          <p>8657393795</p>
-        </div>
-        <div className="email">
-          <EmailOutlinedIcon />
-          <p>jituchoudhary20@gmail.com</p>
-        </div>
-        <div className="home-icon">
-          <HomeOutlinedIcon />
-          <p>
-            Maa Febrication, Near Bank Of India, <br /> Bazargaon, Nagpur-440023
-          </p>
-        </div>
-      </div>
-
-      <div className="connect">
-        <div>
-          <h3>Follow me at-</h3>
-          <a
-            className="linkedin"
-            href="https://linkedin.com/in/jitendra-choudhary-122b05256/"
-            target="_blank"
-          >
-            LinkedIn
-          </a>
-        </div>
-        <div>
-          <a
-            className="github"
-            href="https://github.com/JitendraChoudhary20"
-            target="_blank"
-          >
-            GitHub
-          </a>
-        </div>
-      </div>
     </div>
   );
 };
